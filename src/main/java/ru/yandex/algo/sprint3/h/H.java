c3b950b31307c48f03df79b7bb4b3d83317d9094
Taras <tarasnn@gmail.com>
2022-05-08 12:38:42 +0500

    continue sprint3

--- src/main/java/ru/yandex/algo/sprint3/h/H.java
+++ src/main/java/ru/yandex/algo/sprint3/h/H.java
@@ -13,24 +13,24 @@ public class H {
   // private static final String FILE = "input.txt";
 
   public static void main(String[] args) throws IOException {
-    test1();
-    test2();
-    test3();
+    // test();
+    execute();
   }
 
   private static void execute() throws IOException {
     final String[] elements;
-
     try (final BufferedReader in =
         new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(FILE)), StandardCharsets.UTF_8))) {
       final int elementsCount = Integer.parseInt(in.readLine());
       elements = in.readLine().split(SEPARATOR);
     }
     final String[] sorted = sort(elements);
+    StringBuilder result = new StringBuilder(sorted.length);
     for (String s : sorted) {
-      System.out.print(s + " ");
-      // System.out.print(s);
+      // result.append(s).append("|");
+      result.append(s);
     }
+    System.out.print(result);
   }
 
   private static String[] sort(String[] elements) {
@@ -67,41 +67,53 @@ public class H {
   }
 
   private static int compareInner(String[] lessLength, String[] moreLength) {
-    for (int i = 0; i < lessLength.length; i++) {
-      final int a = Integer.parseInt(lessLength[i]);
+    int a = -1; // всегда будет первая итерация и -1 затрется
+    for (int i = 0; i < moreLength.length; i++) {
       final int b = Integer.parseInt(moreLength[i]);
+
       if (lessLength.length == moreLength.length) {
-        if (a > b) {
-          return 1;
-        } else if (a < b) {
+        a = Integer.parseInt(lessLength[i]);
+        if (a < b) {
           return -1;
+        } else if (a > b) {
+          return 1;
         }
       } else { // lessLength < moreLength
-        if (a >= b) {
+        if (i < lessLength.length) {
+          a = Integer.parseInt(lessLength[i]);
+          if (a < b) {
+            return -1;
+          } else if (a > b) {
+            return 1;
+          }
+        } else if (b == 0) {
           return 1;
-        } else {
+        } else if (b > a) {
           return -1;
+        } else {
+          return 1;
         }
       }
     }
     return 0;
   }
 
-  private static void test1() {
-    String[] lessLength = new String[] {"8"};
-    String[] moreLength = new String[] {"8", "9"};
-    assert compareInner(lessLength, moreLength) == -1;
-  }
+  private static void test() {
 
-  private static void test2() {
-    String[] lessLength = new String[] {"1"};
-    String[] moreLength = new String[] {"1", "0"};
-    assert compareInner(lessLength, moreLength) == 1;
-  }
+    assert compare("8", "88") == 1;
+    assert compare("88", "8") == -1;
 
-  private static void test3() {
-    String[] lessLength = new String[] {"6", "6"};
-    String[] moreLength = new String[] {"6", "8"};
-    assert compareInner(lessLength, moreLength) == -1;
+    assert compare("1", "10") == 1;
+    assert compare("10", "1") == -1;
+
+    assert compare("66", "68") == -1;
+    assert compare("68", "66") == 1;
+
+    assert compare("89", "89") == 0;
+
+    assert compare("89000", "89") == -1;
+
+    assert compare("8", "89") == -1;
+    assert compare("89", "8") == 1;
   }
 }
