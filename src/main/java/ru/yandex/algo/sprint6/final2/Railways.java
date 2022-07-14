03a6fb4bc21dc594f42fa59ca0ec96cb70bea568
Taras <tarasnn@gmail.com>
2022-07-14 23:35:09 +0500

    sprint6 (final2 TL)

--- src/main/java/ru/yandex/algo/sprint6/final2/Railways.java
+++ src/main/java/ru/yandex/algo/sprint6/final2/Railways.java
@@ -7,66 +7,66 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Queue;
 
 public class Railways {
+
+  private static final String R = "R";
 
   //private static final String FILE = "input.txt";
   private static final String FILE = "/home/taras/repoMy/projects/Yandex.algo/src/main/java/ru/yandex/algo/sprint6/final2/input15.txt";
 
   public static void main(String[] args) throws IOException {
-    final Graph graph;
-    final int vertexCount;
     try (final BufferedReader in = new BufferedReader(
         new InputStreamReader(Files.newInputStream(Paths.get(FILE)), StandardCharsets.UTF_8))) {
 
-      vertexCount = Integer.parseInt(in.readLine());
-      graph = buildGraphs(vertexCount, in);
+      final int vertexCount = Integer.parseInt(in.readLine());
+      final Graph graph = buildGraphs(vertexCount, in);
+      if (isMapOptimal(graph, vertexCount)) {
+        System.out.print("YES");
+      } else {
+        System.out.print("NO");
+      }
     }
-    if (isMapOptimal(graph, vertexCount)) {
-      System.out.print("YES");
-    } else {
-      System.out.print("NO");
-    }
+
   }
 
-  private static void recursiveDFS(Graph graph, Vertex vertexV, Colors colors) {
-    colors.markColor(vertexV, Color.GRAY);
+  private static void recursiveDFS(Graph graph, int vertexV, Colors colors) {
+    colors.markColor(vertexV, Colors.GRAY);
     final Queue<Edge> edges = graph.getEdges(vertexV);
     while (null != edges && !edges.isEmpty()) {
-      final Vertex vertexW = edges.poll().getW();
+      final int vertexW = edges.poll().getW();
       if (colors.isGray(vertexW)) {
         throw new NoOptimalMap("The map isn't optimal: " + vertexW);
       } else if (colors.isWhite(vertexW)) {
         recursiveDFS(graph, vertexW, colors);
       }
     }
-    colors.markColor(vertexV, Color.BLACK);
+    colors.markColor(vertexV, Colors.BLACK);
   }
 
   private static boolean isMapOptimal(final Graph graph, int vertexCount) {
     final Colors colors = new Colors(vertexCount);
-    Vertex startVertex = graph.getVertexByNumber(colors.getFirstWhiteVertexNumber());
-    try {
-      while (null != startVertex) {
+    int startVertex = colors.getFirstWhiteVertex();
+    while (-1 != startVertex) {
+      try {
         recursiveDFS(graph, startVertex, colors);
-        startVertex = graph.getVertexByNumber(colors.getFirstWhiteVertexNumber());
+      } catch (NoOptimalMap e) {
+        return false;
       }
-      return true;
-    } catch (NoOptimalMap e) {
-      return false;
+      startVertex = colors.getFirstWhiteVertex();
     }
+    return true;
   }
 
-  private static Graph buildGraphs(int vertexCount, BufferedReader in) throws IOException {
-    final Graph graph = new Graph(vertexCount);
-    for (int i = 1; i < vertexCount; i++) {
+  private static Graph buildGraphs(int intCount, BufferedReader in) throws IOException {
+    final Graph graph = new Graph(intCount);
+    for (int i = 1; i < intCount; i++) {
       final String[] l = in.readLine().split("");
       for (int j = 1; j <= l.length; j++) {
         final String type = l[j - 1];
         final Edge edge;
-        if ("R".equals(type)) {
+        if (R.equals(type)) {
           edge = new Edge(i + j, i);
         } else {
           edge = new Edge(i, i + j);
@@ -88,29 +88,28 @@ public class Railways {
 
 class Colors {
 
-  private final Color[] color;
+  static final int WHITE = 0;
+  static final int GRAY = 1;
+  static final int BLACK = 2;
+  private final int[] color;
 
   public Colors(int capacity) {
-    color = new Color[capacity];
+    color = new int[capacity];
   }
 
-  void markColor(Vertex v, Color c) {
-    color[v.getNumber() - 1] = c;
+  void markColor(int v, int c) {
+    color[v - 1] = c;
   }
 
-  boolean isWhite(Vertex v) {
-    return isWhite(v.getNumber());
+  boolean isWhite(int v) {
+    return color[v - 1] == WHITE;
   }
 
-  boolean isWhite(int vertexNumber) {
-    return color[vertexNumber - 1] == null || color[vertexNumber - 1] == Color.WHILE;
+  boolean isGray(int v) {
+    return color[v - 1] == GRAY;
   }
 
-  boolean isGray(Vertex v) {
-    return color[v.getNumber() - 1] == Color.GRAY;
-  }
-
-  int getFirstWhiteVertexNumber() {
+  int getFirstWhiteVertex() {
     for (int i = 1; i <= color.length; i++) {
       if (isWhite(i)) {
         return i;
@@ -118,12 +117,6 @@ class Colors {
     }
     return -1;
   }
-}
-
-enum Color {
-  WHILE,
-  GRAY,
-  BLACK;
 }
 
 class Graph {
@@ -136,74 +129,36 @@ class Graph {
   }
 
   void addEdgeToVertex(Edge edge) {
-    addEdgeToVertex(edge.getV(), edge);
-  }
-
-  void addEdgeToVertex(Vertex v, Edge edge) {
+    final int v = edge.getV();
     Queue<Edge> edges = getEdges(v);
     if (null == edges) {
       edges = new LinkedList<>();
-      store[v.getNumber() - 1] = edges;
+      store[v - 1] = edges;
     }
     edges.add(edge);
   }
 
-
-  Queue<Edge> getEdges(Vertex v) {
-    final int i = v.getNumber() - 1;
+  Queue<Edge> getEdges(int v) {
+    final int i = v - 1;
     return store[i];
   }
-
-  Vertex getVertexByNumber(int vertexNumber) {
-    if (0 > vertexNumber) {
-      return null;
-    }
-    final Queue<Edge> o = store[vertexNumber - 1];
-    if (Objects.isNull(o)) {
-      return null;
-    }
-    return o.peek().getV();
-  }
-}
-
-class Vertex {
-
-  private final int number;
-
-  public int getNumber() {
-    return number;
-  }
-
-  public Vertex(int number) {
-    this.number = number;
-  }
-
-  @Override
-  public String toString() {
-    return number + "";
-  }
-
 }
 
 class Edge {
 
-  private final Vertex v;
-  private final Vertex w;
+  private final int v;
+  private final int w;
 
-  public Edge(Vertex v, Vertex w) {
+  public Edge(int v, int w) {
     this.v = v;
     this.w = w;
   }
 
-  public Edge(int v, int w) {
-    this(new Vertex(v), new Vertex(w));
-  }
-
-  public Vertex getV() {
+  public int getV() {
     return v;
   }
 
-  public Vertex getW() {
+  public int getW() {
     return w;
   }
 }
