dd2b087fed659bd9fb74882b5d6a7fe7a9c886c3
Taras <tarasnn@gmail.com>
2022-09-01 09:34:44 +0500

    sprint8 (D done)

--- src/main/java/ru/yandex/algo/sprint8/d/D.java
+++ src/main/java/ru/yandex/algo/sprint8/d/D.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
 public class D {
 
   //private static final String FILE = "input.txt";
-  private static final String FILE = "/home/taras/repoMy/projects/Yandex.algo/src/main/java/ru/yandex/algo/sprint8/d/input03.txt";
+  private static final String FILE = "/home/taras/repoMy/projects/Yandex.algo/src/main/java/ru/yandex/algo/sprint8/d/input02.txt";
 
   public static void main(String[] args) throws IOException {
     try (final BufferedReader in = new BufferedReader(
@@ -25,7 +25,7 @@ public class D {
         return;
       }
 
-      final String[][] words = new String[count][];
+      final String[] words = new String[count];
       int minLengthIndex = -1;
       int minLength = Integer.MAX_VALUE;
       for (int i = 0; i < count; i++) {
@@ -34,19 +34,19 @@ public class D {
           minLengthIndex = i;
           minLength = line.length();
         }
-        words[i] = line.split("");
+        words[i] = line;
       }
 
       System.out.println(doCount(words, minLengthIndex));
     }
   }
 
-  private static int doCount(String[][] words, int minLengthIndex) {
+  private static int doCount(String[] words, int minLengthIndex) {
     int result = 0;
-    final String[] minWord = words[minLengthIndex];
-    for (int j = 0; j < minWord.length; j++) {
+    final String minWord = words[minLengthIndex];
+    for (int j = 0; j < minWord.length(); j++) {
       for (int i = 0; i < words.length; i++) {
-        if (!minWord[j].equals(words[i][j])) {
+        if (!minWord.substring(j, j + 1).equals(words[i].substring(j, j + 1))) {
           return result;
         }
       }
