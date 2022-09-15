768979f995d6d1e8c472b8e065d932369afa1aa7
taras <tarasnn@gmail.com>
2022-09-15 09:57:05 +0500

    sprint8 (final2 done)

--- src/main/java/ru/yandex/algo/sprint8/final2/CheatSheet.java
+++ src/main/java/ru/yandex/algo/sprint8/final2/CheatSheet.java
@@ -1,3 +1,4 @@
+// 70463799
 package ru.yandex.algo.sprint8.final2;
 
 import java.io.BufferedReader;
@@ -8,11 +9,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
+/**
+ * Сначала строим бор. Сложность по вычислению построения бора составляет O(n), где n - кол-во символов во всех словах. Сложность по памяти
+ * O(n). Затем проверяем, можно ли создать строку с данным индексом. Каждый индекс требует обход бора. Сложность по вычислению O(n^2), где n
+ * кол-во символов в данной строке, сложность по памяти O(n).
+ */
 
 public class CheatSheet {
 
   //private static final String FILE = "input.txt";
-  private static final String FILE = "/home/taras/repoMy/projects/Yandex.algo/src/main/java/ru/yandex/algo/sprint8/final2/input01.txt";
+  private static final String FILE = "/home/taras/Code/repoMy/projects/Yandex.algo/src/main/java/ru/yandex/algo/sprint8/final2/input03.txt";
 
   public static void main(String[] args) throws IOException {
     try (final BufferedReader in = new BufferedReader(
@@ -24,17 +30,15 @@ public class CheatSheet {
         System.out.println("NO");
         return;
       }
-
       final Node root = buildTree(in, count);
-
       System.out.println(isSplit(line, root) ? "YES" : "NO");
     }
   }
 
   private static Node buildTree(BufferedReader in, int count) throws IOException {
-    Node root = new Node();
+    final Node root = new Node();
     for (int j = 1; j <= count; j++) {
-      String word = in.readLine();
+      final String word = in.readLine();
       Node node = root;
       for (int i = 0; i < word.length(); i++) {
         final String s = word.substring(i, i + 1);
@@ -52,7 +56,7 @@ public class CheatSheet {
   }
 
   private static boolean isSplit(String line, Node root) {
-    boolean[] dp = new boolean[line.length()];
+    final boolean[] dp = new boolean[line.length() + 1];
     dp[0] = true;
     for (int i = 0; i < line.length(); i++) {
       Node node = root;
@@ -61,15 +65,18 @@ public class CheatSheet {
           if (node.terminal > 0) {
             dp[j] = true;
           }
-          String s = line.substring(j, j + 1);
-          if (j == line.length() || (null != node.next && null != node.next.get(s))) {
+          if (j == line.length() || (null == node.next)) {
+            break;
+          }
+          final String s = line.substring(j, j + 1);
+          if (null == node.next.get(s)) {
             break;
           }
           node = node.next.get(s);
         }
       }
     }
-    return dp[line.length() - 1];
+    return dp[line.length()];
   }
 
 
